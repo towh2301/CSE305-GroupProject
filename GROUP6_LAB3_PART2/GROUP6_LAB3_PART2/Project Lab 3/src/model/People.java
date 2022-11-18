f4613655b62c4aac3c42eb4cc45829d7ad611672
Bui Hoang Huy <74142615+angh789@users.noreply.github.com>
2022-11-18 14:21:47 +0700

    Add files via upload

---
+++ GROUP6_LAB3_PART2/GROUP6_LAB3_PART2/Project Lab 3/src/model/People.java
@@ -0,0 +1,63 @@
+package model;
+
+public class People {
+    String id, firstName, surName, dateOfBirth, gender, email;
+
+    public People(String id, String firstName, String surName, String dateOfBirth, String gender, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.surName = surName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
---
+++ GROUP6_LAB3_PART2/GROUP6_LAB3_PART2/Project Lab 3/src/model/Staff.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Staff extends People {
+
+    public Staff(String id, String firstName, String surName, String dateOfBirth, String gender, String email) {
+        super(id, firstName, surName, dateOfBirth, gender, email);
+    }
+    
+}
